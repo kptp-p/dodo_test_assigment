365811c26bd78877605a1d68677dfd06398084d1
unknown <paranin.maxim@mail.ru>
2026-03-31 18:39:51 +0300

    add stable_state

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ def select_roi_with_mouse(video):
     return roi
 
 
+def detect_people_in_roa(results):
+    for result in results:
+        if 0 in result.boxes.cls:
+            return True
+        return False
+
+
 def open_video(path: str):
     model = YOLO("yolo26n.pt")
     video = cv2.VideoCapture(path)
@@ -16,7 +23,13 @@ def open_video(path: str):
     if not video.isOpened():
         print('Video not found')
 
-    x, y, h, w = select_roi_with_mouse(video) 
+    x, y, w, h = select_roi_with_mouse(video)
+
+    current_state: bool = False
+    pervious_state: bool = False
+    stable_state: bool = False
+    counter = 0
+    current_color = (0, 0, 255)
 
     while video.isOpened():
         ret, frame = video.read()
@@ -25,15 +38,30 @@ def open_video(path: str):
             print("Can't recieve frame")
             break
 
-        roi_frame = frame[y:y+w, x:x+h]
+        roi_frame = frame[y:y+h, x:x+w]
 
-        results = model(roi_frame)
-        for result in results:
-            if 0 in result.boxes.cls:
-                print('chel')
-                cv2.rectangle(frame, (x, y), (x+h, y+w), (0, 255, 0), 2)
-            else:
-                cv2.rectangle(frame, (x, y), (x+h, y+w), (0, 0, 255), 2)
+        results = model.predict(roi_frame)
+        pervious_state = current_state
+
+        if detect_people_in_roa(results):
+            current_state = True
+        else:
+            current_state = False
+
+        if current_state == pervious_state:
+            counter += 1
+        else:
+            counter = 0
+
+        if counter >= 10:
+            stable_state = current_state
+
+        if stable_state:
+            current_color = (0, 255, 0)
+        elif not stable_state:
+            current_color = (0, 0, 255)
+
+        cv2.rectangle(frame, (x, y), (x+w, y+h), current_color, 2)
         cv2.imshow("Orig", frame)
 
         if cv2.waitKey(1) == ord('q'):
@@ -43,4 +71,4 @@ def open_video(path: str):
     cv2.destroyAllWindows()
 
 
-open_video("2.mp4")
+open_video("1.mp4")
